937a1858a963ef4e82d7a1c308d40ac8ab5f3ac5
calinnc <92983132+calinnc@users.noreply.github.com>
2021-10-29 03:02:42 -0700

    Add files via upload

---
+++ Client2/include/headers.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <glib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <gio/gio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>

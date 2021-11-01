8563ef59a60d7e865265921fd7e0247bd9bcf1df
sorinaioja <sorina_ioja@yahoo.com>
2021-11-01 10:04:43 +0200

    client1 updates

--- client1/src/client.c
+++ client1/src/client.c
@@ -1,9 +1,41 @@
 #include "../include.h"
 
+void send_file(GSocket* socket){
+
+    printf("enter file name: \n");
+    char fileName[100];
+
+    scanf("%s",fileName);
+    
+    int fd = open(fileName,O_RDONLY);
+    char text[1024];
+
+    if(fd<0){
+        strcpy(text, "Wrong path or file doesn't exist\n");
+        printf("%s\n", text);
+    }
+    else{
+        read(fd,text,1024);
+    }
+        
+    g_socket_send(socket,text,1024,0,0);
+
+}
+void receive_text(GSocket* socket){
+
+    char text[1024];
+    g_socket_receive(socket,text,1024,0,0);
+
+    printf("text received: \n");
+    printf("%s\n",text);
+
+}
+
 int main(int argc,char** argv)
 {
     int port = atoi(argv[1]);
     struct sockaddr_in server;
+    GError *error = NULL;
 
     server.sin_port=htons(port);
     server.sin_family=AF_INET;
@@ -14,13 +46,13 @@ int main(int argc,char** argv)
 
     if(g_socket_connect(socket,address,0,0)==0)
     {
-        printf("Connect error\n");
+        printf("Connecting error\n");
         exit(1);
     }
 
     //Client 1 decides if he sends or receives info
     int nr_client = 1;
-    g_socket_send(socket,(gchar*)&nr_client,4,0,0);
+    g_socket_send(socket,(gchar*)&nr_client,sizeof(int),0,0);
 
     char request[100];
     printf("Logged in -> enter command: send or receive\n");
@@ -28,21 +60,10 @@ int main(int argc,char** argv)
     g_socket_send(socket,request,100,0,0);
 
     if(strcmp(request,"send")==0)
-    {
-        printf("enter file name: \n");
-        char fileName[100];
-        scanf("%s",fileName);
-        int fd = open(fileName,O_RDONLY);
-        char text[1024];
-        read(fd,text,1024);
-        g_socket_send(socket,text,1024,0,0);
-    }
+        send_file(socket);
     else
-    {
-        char text[1024];
-        g_socket_receive(socket,text,1024,0,0);
-        printf("text received: \n");
-        printf("%s\n",text);
-    }
+        receive_text(socket);
+
+
     return 0;
 }

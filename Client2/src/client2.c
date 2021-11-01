a97a2cbbe2d56373e2406b1fd03118e75d71685e
calinnc <chereches.calin98@gmail.com>
2021-11-01 03:24:14 -0700

    modificare2 Client2

--- Client2/src/client2.c
+++ Client2/src/client2.c
@@ -1,40 +1,37 @@
-#pragma once
-#include <stdio.h>
-#include <glib.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <gio/gio.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netinet/ip.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <netdb.h>
-//#include "../headers.h"
+#include "../include.h"
 
-#define PORT 8080
+//#define PORT 8080
 
 void func(GSocket* sockfd)
 {
     char msg[100];
-    g_socket_receive(socket,msg,100,NULL,NULL);
-
+    g_socket_receive(sockfd,msg,100,0,0);
+    printf("Urmatoarea actiune: %s\n", msg);
     if(strcmp(msg, "send") == 0)
     {
         printf("Fisier text trimis catre server...\n");
+        printf("Introduceti numele fisierului: \n");
+        char fileName[100];
+        scanf("%s",fileName);
+        int fd = open(fileName,O_RDONLY);
         char buffer1[1024];
-        FILE *fp = fopen("/home/calinnc/Documents/Exercises/Client1/misc/Mesaj", "r");
+        read(fd,buffer1,1024);
+
+        g_socket_send(sockfd,buffer1,1024,0,0);
+
+
+        //VARIANTA PENTRU MAI TARZIU.........
+        /*FILE *fp = fopen("/home/calinnc/Documents/Exercises/Client2/misc/Mesaj", "r");
         while (fgets(buffer1, sizeof(buffer1), fp)) 
-            g_socket_send(socket, buffer1, 1024, NULL, NULL);
-        fclose(fp);
+            g_socket_send(sockfd, buffer1, 1024, 0, 0);
+        fclose(fp);*/ 
+
     }
     else 
         if(strcmp(msg, "receive") == 0)
         {
         char buffer2[1024];
-        g_socket_receive(socket, buffer2, 1024, NULL, NULL);
+        g_socket_receive(sockfd, buffer2, 1024, 0, 0);
         printf("Textul primit: \n");
         printf("%s\n", buffer2); 
         }
@@ -45,21 +42,22 @@ void func(GSocket* sockfd)
 int main(int argc,char** argv)
 {
     struct sockaddr_in serveraddr;
-    
-    //creare socket si verificare
-    GSocket* socket = g_socket_new(G_SOCKET_FAMILY_IPV4,G_SOCKET_TYPE_STREAM,G_SOCKET_PROTOCOL_TCP, NULL);
-    GSocketAddress* address = g_socket_address_new_from_native(&serveraddr,sizeof(serveraddr));
-
-    if(socket == NULL)
-        printf("Eroare la crearea socket-ului!\n");
-    else
-        printf("Socket creat cu succes!\n");
+    int PORT = atoi(argv[1]);
 
     serveraddr.sin_port=htons(PORT);
     serveraddr.sin_family = AF_INET;
     serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    if(g_socket_connect(socket,address, 0, 0) == NULL)
+    //creare socket si verificare
+    GSocket* socket = g_socket_new(G_SOCKET_FAMILY_IPV4,G_SOCKET_TYPE_STREAM,G_SOCKET_PROTOCOL_TCP, NULL);
+    GSocketAddress* address = g_socket_address_new_from_native(&serveraddr,sizeof(serveraddr));
+
+    if(socket == 0)
+        printf("Eroare la crearea socket-ului!\n");
+    else
+        printf("Socket creat cu succes!\n");
+
+    if(g_socket_connect(socket,address, 0, 0) == 0)
     {
         printf("Conectare esuata! \n");
         exit(1);
@@ -68,7 +66,8 @@ int main(int argc,char** argv)
         printf("Conectare reusita!\n");
 
     int client = 2;
-    g_socket_send(socket, &client, 4, NULL, NULL);
+    g_socket_send(socket, (gchar*)&client, 4, 0, 0);
+
     func(socket);
 
     return 0;

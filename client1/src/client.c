54420a622d765136210d56f7b4c4b5167d10d2c3
sorinaioja <sorina_ioja@yahoo.com>
2021-11-08 16:31:34 +0200

    menu log and threads

--- client1/src/client.c
+++ client1/src/client.c
@@ -1,5 +1,44 @@
 #include "../include.h"
 
+
+void* command_menu(void* arg){
+
+    GSocket* socket = (GSocket*) arg;
+
+    int command_number;
+    int from_client_id;
+
+    printf("Enter command number:\n1.receive from clientId (you have to insert it) \n2.send\n");
+
+    scanf("%d %d",&command_number, &from_client_id);
+
+    switch (command_number)
+    {
+        case 1:
+            printf("\nYou will receive the file soon");
+            g_socket_send(socket,"receive text",100,0,0);
+            g_socket_send(socket, (gchar*)&from_client_id, sizeof(int), 0,0);
+            receive_text(socket);
+            break;
+        
+        case 2:
+            printf("\nThe file will be sent soon");
+            g_socket_send(socket,"send text",100,0,0);
+            send_text(socket);
+            break;
+        
+        default:
+            printf("Can't solve the request\n");
+            break;
+    }   
+}
+
+void* respond(void* arg){
+
+    GSocket* socket = (GSocket*) arg;
+    send_text(socket);
+}
+
 int main(int argc,char** argv)
 {
     int port = atoi(argv[1]);
@@ -19,32 +58,18 @@ int main(int argc,char** argv)
             printf("Connecting error\n");
             exit(1);
         }
+        
+        int client_id;
+        g_socket_receive(socket, (gchar*)&client_id, sizeof(int), 0,0);
 
-        int command_number;
-        int from_client_id;
-        printf("Enter command number:\n1.receive from clientId (you have to insert it) \n2.send\n");
-        scanf("%d %d",&command_number, &from_client_id);
+        GThread* tid1;
+        GThread* tid2;
 
-        switch (command_number)
-        {
-        case 1:
-            printf("\nYou will receive the file soon");
-            g_socket_send(socket,"receive text",100,0,0);
-            g_socket_send(socket, (gchar*)&from_client_id, sizeof(int), 0,0);
-            receive_text(socket);
-            break;
-        
-        case 2:
-            printf("\nThe file will be sent soon");
-            g_socket_send(socket,"send text",100,0,0);
-            send_text(socket);
-            break;
-        
-        default:
-            printf("Can't solve the request\n");
-            break;
-        }      
-        
+        tid1 = g_thread_new(0,command_menu,socket);
+        tid2 = g_thread_new(0,respond,socket);
+
+        //g_thread_join(tid1);
+        //g_thread_join(tid2);         
     }
     
     return 0;

efa547ff90135703b37b375ccf6d53e5735566a9
sorinaioja <sorina_ioja@yahoo.com>
2021-11-08 13:29:26 +0200

    client 1 receive path changes

--- client1/src/client.c
+++ client1/src/client.c
@@ -1,11 +1,9 @@
 #include "../include.h"
 
-void send_file(GSocket* socket){
+void send_text(GSocket* socket){
 
-    printf("enter file name: \n");
     char fileName[100];
-
-    scanf("%s",fileName);
+    g_socket_receive(socket,fileName,100,0,0);
     
     int fd = open(fileName,O_RDONLY);
     char text[1024];
@@ -21,9 +19,11 @@ void send_file(GSocket* socket){
     g_socket_send(socket,text,1024,0,0);
 
 }
-void receive_text(GSocket* socket){
+void receive_text(GSocket* socket, char *filepath){
 
     char text[1024];
+    bzero(text,1024);
+    g_socket_send(socket, filepath, strlen(filepath),0,0);
     g_socket_receive(socket,text,1024,0,0);
 
     printf("text received: \n");
@@ -44,35 +44,65 @@ int main(int argc,char** argv)
     GSocket* socket = g_socket_new(G_SOCKET_FAMILY_IPV4,G_SOCKET_TYPE_STREAM,G_SOCKET_PROTOCOL_TCP,NULL);
     GSocketAddress* address = g_socket_address_new_from_native(&server,sizeof(server));
 
-    if(g_socket_connect(socket,address,0,0)==0)
-    {
-        printf("Connecting error\n");
-        exit(1);
-    }
 
-    //Client 1 decides if he sends or receives info
-    int nr_client = 1;
-    g_socket_send(socket,(gchar*)&nr_client,sizeof(int),0,0);
+    while(1){
 
-    char request[100];
-    printf("Logged in -> enter command: send or receive\n");
-    scanf("%s",request);
-    g_socket_send(socket,request,100,0,0);
+        if(g_socket_connect(socket,address,0,0)==0)
+        {
+            printf("Connecting error\n");
+            exit(1);
+        }
 
-    //Primesc de la server mesaj daca se poate face sau nu transferul
-    char message[100];
-    g_socket_receive(socket, message,100,0,0);
+        //Client 1 decides if he sends or receives info
+        int nr_client = 1;
+        g_socket_send(socket,(gchar*)&nr_client,sizeof(int),0,0);
 
-    if(strcmp(message, "success")==0){
-        
-        if(strcmp(request,"send")==0)
-            send_file(socket);
+        char request[100];
+        printf("Enter command number:\n1.receive filename \n2.send\n");
+        scanf("%[^\n]",request);
+
+        char command[8];
+        char filepath[30];
+
+        char * pch;
+        int i = 0;
+        pch = strtok (request," \n");
+        while (pch != NULL)
+        {
+            if(i == 0){
+                strcpy(command,pch);
+            }
+            if( i == 1)
+                strcpy(filepath, pch);
+            i++;
+            pch = strtok (NULL, " \n");
+        }
+
+        if(i < 1 && strcmp(command,"receive")==0){
+            printf("Wrong number of arguments\n");
+        }
+
+        g_socket_send(socket,command,100,0,0);
+
+
+        char message[100];
+        g_socket_receive(socket, message,100,0,0);
+
+        if(strcmp(message, "success")==0){
+            if(strcmp(command,"send")==0){
+                    send_text(socket);
+                    strcpy(request, "\n");
+                }       
+                else{
+                    receive_text(socket, filepath);
+                    strcpy(request, "\n");
+                }
+        }
         else
-            receive_text(socket);
-
+            printf("Can't solve the request\n");
+        
     }
-    else
-        printf("Can't solve the request\n");
+    
 
     return 0;
 }

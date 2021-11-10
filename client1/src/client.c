3762d110b707d6c582c0a3b162178ac3f2084d82
Matei <matei.ll64@gmail.com>
2021-11-10 01:20:00 -0800

    .

--- client1/src/client.c
+++ client1/src/client.c
@@ -15,14 +15,14 @@ void* command_menu(void* arg){
     switch (command_number)
     {
         case 1:
-            printf("\nYou will receive the file soon");
+            printf("You will receive the file soon\n");
             g_socket_send(socket,"receive text",100,0,0);
             g_socket_send(socket, (gchar*)&from_client_id, sizeof(int), 0,0);
             receive_text(socket);
             break;
         
         case 2:
-            printf("\nThe file will be sent soon");
+            printf("The file will be sent soon\n");
             g_socket_send(socket,"send text",100,0,0);
             send_text(socket);
             break;
@@ -56,19 +56,23 @@ int main(int argc,char** argv)
     GSocket* socket = g_socket_new(G_SOCKET_FAMILY_IPV4,G_SOCKET_TYPE_STREAM,G_SOCKET_PROTOCOL_TCP,NULL);
     GSocketAddress* address = g_socket_address_new_from_native(&server,sizeof(server));
 
-    while(1){
-
-        if(g_socket_connect(socket,address,0,0)==0)
+    if(g_socket_connect(socket,address,0,0)==0)
         {
             printf("Connecting error\n");
             exit(1);
         }
 
-        printf("\n\nConnected to server");
+    printf("Connected to server\n");
 
-        int client_id;
+    int clientType = 2;
+        g_socket_send(socket,&clientType,4,0,0);
+
+    int client_id;
         g_socket_receive(socket, (gchar*)&client_id, sizeof(int), 0,0);
-        printf("\nI am client number %d", client_id);
+        printf("I am client number %d\n", client_id);
+
+    while(1){
+
 
         GThread* tid1;
         GThread* tid2;

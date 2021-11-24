5142e7c5a524c3217871695bfdb3f65d4ce60e16
sorinaioja <sorina_ioja@yahoo.com>
2021-11-24 13:30:44 +0200

    receive script functonality

--- client1/src/client.c
+++ client1/src/client.c
@@ -137,7 +137,8 @@ int main(int argc, char **argv)
     printf("Connected to server\n");
 
     //test_compressed();
-    test_extract_string();
+    //test_hash();
+    //test_extract_string();
 
     int clientType = 1;
     g_socket_send(socket, &clientType, 4, 0, 0); //send the type of the client
